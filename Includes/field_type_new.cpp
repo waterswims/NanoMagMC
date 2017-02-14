1e0f0a0f9304486a00bc816f4e77e4d40a6e273c
J.M.Waters <J.M.Waters@soton.ac.uk>
2017-02-14 16:31:28 +0000

    Completed fireld 2d

--- Includes/field_type_new.cpp
+++ Includes/field_type_new.cpp
@@ -5,6 +5,16 @@
 extern mkl_irand st_rand_int;
 extern mkl_drand st_rand_double;
 
+int boundmovedown(int test, int limit)
+{
+    return ((test%limit)+limit)%limit;
+}
+
+int boundmoveup(int test, int limit)
+{
+    return test%limit;
+}
+
 void field_type::rand_spin_h(double &x, double &y, double &z)
 {
     double phi = st_rand_double.gen()*2*pi;
@@ -68,7 +78,7 @@ field_cluster_h::field_cluster_h(field_type& other)
     insize = other.get_insize();
     totsize = other.get_totsize();
     double* xoth, yoth, zoth;
-    get_1dfield_h(xoth, yoth, zoth);
+    other.get_1dfield_h(xoth, yoth, zoth);
     spinx = deep_copy_1darr(insize, xoth);
     spiny = deep_copy_1darr(insize, yoth);
     spinz = deep_copy_1darr(insize, zoth);
@@ -82,7 +92,7 @@ field_cluster_h::field_cluster_h(const field_cluster_h& other)
     insize = other.get_insize();
     totsize = other.get_totsize();
     double* xoth, yoth, zoth;
-    get_1dfield_h(xoth, yoth, zoth);
+    other.get_1dfield_h(xoth, yoth, zoth);
     spinx = deep_copy_1darr(insize, xoth);
     spiny = deep_copy_1darr(insize, yoth);
     spinz = deep_copy_1darr(insize, zoth);
@@ -96,7 +106,7 @@ field_cluster& field_cluster_h::operator=(field_cluster& other)
     insize = other.get_insize();
     totsize = other.get_totsize();
     double* xoth, yoth, zoth;
-    get_1dfield_h(xoth, yoth, zoth);
+    other.get_1dfield_h(xoth, yoth, zoth);
     spinx = deep_copy_1darr(insize, xoth);
     spiny = deep_copy_1darr(insize, yoth);
     spinz = deep_copy_1darr(insize, zoth);
@@ -113,8 +123,8 @@ field_cluster_h::~field_cluster()
 void field_cluster_h::h_access(vector<int>& postion, vector<double>& out)
 {
     out[0] = spinx[position[0]];
-    out[1] = spiny[position[1]];
-    out[2] = spinz[position[2]];
+    out[1] = spiny[position[0]];
+    out[2] = spinz[position[0]];
 }
 
 void field_cluster_h::h_next(bool &finish, vector<int> &pos, vector<double> &out)
@@ -133,4 +143,185 @@ void field_cluster_h::get_1dfield_h(double* &x, double* &y, double* &z)
     x = spinx;
     y = spiny;
     z = spinz;
+}
+
+field_2d_h::field_2d_h()
+{
+    dim = 2;
+    periodic = true;
+    ft = 2;
+}
+
+field_2d_h::field_2d_h(int size, bool isperio)
+{
+    dim = 2;
+    periodic = isperio;
+    insize = size;
+    ft = 2;
+    if(periodic)
+    {
+        totsize = insize;
+    }
+    else
+    {
+        totsize = insize + 2;
+    }
+    spinx = alloc_2darr(totsize, totsize);
+    spiny = alloc_2darr(totsize, totsize);
+    spinz = alloc_2darr(totsize, totsize);
+}
+
+field_2d_h::field_2d_h(field_type& other)
+{
+    ft = 2;
+    if(other.ft != 2)
+    {
+        cout << "Cannot copy from other field type" << endl;
+        exit(104);
+    }
+    dim = 2;
+    insize = other.get_insize();
+    totsize = other.get_totsize();
+    periodic = other.get_perio();
+    double** xoth, yoth, zoth;
+    other.get_2dfield_h(xoth, yoth, zoth);
+    spinx = deep_copy_2darr(insize, xoth);
+    spiny = deep_copy_2darr(insize, yoth);
+    spinz = deep_copy_2darr(insize, zoth);
+}
+
+field_2d_h::field_2d_h(const field_2d_h& other)
+{
+    ft = 2;
+    dim = 2;
+    insize = other.get_insize();
+    totsize = other.get_totsize();
+    periodic = other.get_perio();
+    double** xoth, yoth, zoth;
+    other.get_2dfield_h(xoth, yoth, zoth);
+    spinx = deep_copy_2darr(insize, xoth);
+    spiny = deep_copy_2darr(insize, yoth);
+    spinz = deep_copy_2darr(insize, zoth);
+}
+
+field_2d_h& field_2d_h::operator=(field_2d_h& other)
+{
+    ft = 2;
+    dim = 2;
+    insize = other.get_insize();
+    totsize = other.get_totsize();
+    periodic = other.get_perio();
+    double** xoth, yoth, zoth;
+    other.get_2dfield_h(xoth, yoth, zoth);
+    spinx = deep_copy_2darr(insize, xoth);
+    spiny = deep_copy_2darr(insize, yoth);
+    spinz = deep_copy_2darr(insize, zoth);
+    return *this;
+}
+
+field_2d_h::~field_2d_h()
+{
+    dealloc_2darr(totsize, totsize, spinx);
+    dealloc_2darr(totsize, totsize, spiny);
+    dealloc_2darr(totsize, totsize, spinz);
+}
+
+void field_2d_h::h_access(vector<int>& position, vector<double>& out)
+{
+    out[0] = spinx[position[0]][position[1]];
+    out[1] = spiny[position[0]][position[1]];
+    out[2] = spinz[position[0]][position[1]];
+}
+
+void field_2d_h::h_next(bool &finish, vector<int> &pos, vector<double> &out)
+{
+    int start = 0;
+    int end = totsize;
+    if(!periodic)
+    {
+        start++;
+        end--;
+    }
+    this->h_access(pos, out);
+    pos[1]++;
+    if(pos[1] == end)
+    {
+        pos[1] = start;
+        pos[0]++;
+        if(pos[0] == end;)
+        {
+            pos[0] = start;
+            finish = true;
+        }
+    }
+    return out;
+}
+
+void field_2d_h::fill_ghost()
+{
+    if(!periodic)
+    {
+        for(int i=0; i < totsize; i++)
+        {
+            spinx[i][0] = 0;
+            spiny[i][0] = 0;
+            spinz[i][0] = 0;
+            spinx[0][i] = 0;
+            spiny[0][i] = 0;
+            spinz[0][i] = 0;
+            spinx[i][totsize-1] = 0;
+            spiny[i][totsize-1] = 0;
+            spinz[i][totsize-1] = 0;
+            spinx[totsize-1][i] = 0;
+            spiny[totsize-1][i] = 0;
+            spinz[totsize-1][i] = 0;
+        }
+    }
+}
+
+int field_2d_h::findnum()
+{
+    int c = 0;
+    for(int i = 0; i<totsize; i++)
+    {
+        for(int j = 0; j<totsize; j++)
+        {
+            if(spinx[i][j]!=0 || spiny[i][j]!=0 || spinz[i][j]!=0)
+            {
+                c++;
+            }
+        }
+    }
+    return c;
+}
+
+void field_2d_h::get_2dfield_h(double** &x, double** &y, double** &z)
+{
+    x = spinx;
+    y = spiny;
+    z = spinz;
+}
+
+void field_2d_h::h_adjacent(vector<int>& position, double** &out)
+{
+    out = alloc_2darr(4, 3);
+    int dirsx[4], dirsy[4];
+    dirsx[0] = boundmovedown(position[0] - 1, totsize);
+    dirsx[1] = boundmoveup(position[0] + 1, totsize);
+    dirsx[2] = position[0];
+    dirsx[3] = position[0];
+
+    dirsy[0] = position[1];
+    dirsy[1] = position[1];
+    dirsy[2] = boundmovedown(position[1] - 1, totsize);
+    dirsy[3] = boundmoveup(position[1] + 1, totsize);
+
+    for (int i = 0; i < 4; i++)
+    {
+        out[i][0] = spinx[dirsx[i]][dirsy[i]];
+        out[i][1] = spiny[dirsx[i]][dirsy[i]];
+        out[i][2] = spinz[dirsx[i]][dirsy[i]];
+    }
+
+    return out;
 }
--- Includes/field_type_new.hpp
+++ Includes/field_type_new.hpp
@@ -16,16 +16,16 @@ public:
     field_type(){}
     ~field_type(){}
     virtual void i_access(vector<int>& postion, int &out){}
-    virtual void i_adjacent(vector<int>& position, vector<int>& out){}
+    virtual void i_adjacent(vector<int>& position, int* &out){}
     virtual void h_access(vector<int>& postion, vector<double>& out){}
-    virtual void h_adjacent(vector<int>& position, vector<vector<double> >& out){}
+    virtual void h_adjacent(vector<int>& position, double** &out){}
     virtual void i_next(bool &finish, vector<int> &pos, int &out){}
     virtual void h_next(bool &finish, vector<int> &pos, vector<double> &out){}
     int get_insize(){return insize;}
     int get_totsize(){return totsize;}
     bool get_perio(){return periodic;}
     int get_dim(){return dim;}
-    virtual void fill_ghost(int num){}
+    virtual void fill_ghost(){}
     virtual void new_mem(){}
     virtual int findnum(){return 0;}
     virtual void get_2dfield_i(int** &x){}
@@ -51,9 +51,30 @@ public:
     ~field_cluster();
     void h_access(vector<int>& postion, vector<double>& out);
     void h_next(bool &finish, vector<int> &pos, vector<double> &out);
-    field_cluster& operator=(field_cluster& other);
+    field_cluster_h& operator=(field_cluster_h& other);
     int findnum(){return insize;}
     void get_1dfield_h(double* &x, double* &y, double* &z);
+};
+
+class field_2d_h: public field_type
+{
+protected:
+    double** spinx;
+    double** spiny;
+    double** spinz;
+public:
+    field_2d_h();
+    field_2d_h(int size, bool isperio);
+    field_2d_h(field_type& other);
+    field_2d_h(const field_2d_h& other);
+    ~field_2d_h();
+    void h_access(vector<int>& position, vector<double>& out);
+    void h_next(bool &finish, vector<int> &pos, vector<double> &out);
+    void fill_ghost();
+    field_2d_h& operator=(field_2d_h& other);
+    int findnum();
+    void get_2dfield_h(double** &x, double** &y, double** &z);
+    void h_adjacent(vector<int>& position, vector<vector<double> >& out);
 };
 
 #endif
