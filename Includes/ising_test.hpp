28ce5ebfc14e27890b6fc68985d6068bcb1abe35
J.M.Waters <J.M.Waters@soton.ac.uk>
2017-02-27 15:43:01 +0000

    Finished ising model tests

--- Includes/ising_test.hpp
+++ Includes/ising_test.hpp
@@ -238,4 +238,65 @@ TEST(Ising_model, 3d_ferromagnetic_dE)
     EXPECT_EQ(12, hamil.dE(&field, pos));
 }
 
+TEST(Ising_model, 3d_antiferromagnetic_energy_zero_field)
+{
+    field_3d_i field = gen_3d_ising_afm();
+    ham_ising hamil(0, 1);
+    hamil.init_dim(&field);
+    EXPECT_EQ(2700, hamil.calc_E(&field));
+}
+
+TEST(Ising_model, 3d_antiferromagnetic_energy_ext_field)
+{
+    field_3d_i field = gen_3d_ising_afm();
+    ham_ising hamil(0.1, 1);
+    hamil.init_dim(&field);
+    EXPECT_EQ(2700, hamil.calc_E(&field));
+}
+
+TEST(Ising_model, 3d_antiferromagnetic_mag)
+{
+    field_3d_i field = gen_3d_ising_afm();
+    ham_ising hamil(0, 1);
+    hamil.init_dim(&field);
+    EXPECT_EQ(0, hamil.calc_M(&field)[0]);
+}
+
+TEST(Ising_model, 3d_antiferromagnetic_submag)
+{
+    field_3d_i field = gen_3d_ising_fm();
+    ham_ising hamil(0, 1);
+    hamil.init_dim(&field);
+    EXPECT_EQ(500, hamil.calc_subM(&field, 1)[0]);
+}
+
+TEST(Ising_model, 3d_antiferromagnetic_dE)
+{
+    field_3d_i field = gen_3d_ising_fm();
+    ham_ising hamil(0, 1);
+    hamil.init_dim(&field);
+    vector<int> pos(3,5);
+    EXPECT_EQ(12, hamil.dE(&field, pos));
+}
+
+TEST(Ising_model, 3d_dE_consist)
+{
+    field_3d_i field = gen_3d_ising_fm();
+    ham_ising hamil(0, 1);
+    hamil.init_dim(&field);
+    vector<int> pos(3);
+    int old_E = hamil.calc_E(&field);
+    for(int i = 0; i < 1000; i++)
+    {
+        pos[0] = int(st_rand_double.gen()*10 + 1);
+        pos[1] = int(st_rand_double.gen()*10 + 1);
+        pos[3] = int(st_rand_double.gen()*10 + 1);
+        int dE = hamil.dE(&field, pos);
+        field.change_to_test(pos, &hamil);
+        int new_E = hamil.calc_E(&field);
+        EXPECT_EQ(old_E + dE, new_E);
+        old_E = new_E;
+    }
+}
+
 #endif
