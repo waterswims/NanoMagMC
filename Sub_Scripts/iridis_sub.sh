88ef3b874ea6fd57e2fdfac3597928f8f6e7a061
Jonathon Waters <j.m.waters@soton.ac.uk>
2017-12-16 14:06:08 +0000

    New submission scripts

--- Sub_Scripts/iridis_sub.sh
+++ Sub_Scripts/iridis_sub.sh
@@ -3,7 +3,7 @@
 # set default resource requirements for job
 # - these can be overridden on the qsub command line (this is for a 4 hour job)
 #PBS -l walltime=60:00:00
-#PBS -l nodes=32:ppn=16
+#PBS -l nodes=4:ppn=16
 #PBS -j oe
 
 # Change to the directory from which the job was submitted.
@@ -13,4 +13,4 @@ source load_compilers.sh
 nprocs=`wc -l $PBS_NODEFILE | awk '{ print $1 }'`
 
 # Run my_prog taking input from file input_file and send output to output_file
-mpirun -bootstrap rsh -f $PBS_NODEFILE -n $nprocs ./run INPUT
+mpirun -n 51 ./run INPUT.dat
