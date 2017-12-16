3c4467fae2ec01b0accce1fbbdfcb1407d37d27b
Jonathon Waters <jmw2g14@soton.ac.uk>
2017-12-16 13:39:08 +0000

    Removed extra submission scripts

--- Sub_Scripts/f_setup.sh
+++
@@ -1,8 +0,0 @@
-module swap PrgEnv-cray PrgEnv-intel
-make clean run
-
-for FIELD in 0.25 0.5 0.75 1 1.25 1.5 1.75 2 2.25 2.5 2.75 3
-do
-	cp ARC_INPUT.dat ARC_INPUT_"$FIELD"
-	sed -i "s/MAGFIELD .*/MAGFIELD ${FIELD}/g" ARC_INPUT_"$FIELD"
-done
